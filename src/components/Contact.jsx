2a509d01fb577ade7f0f6fb19526951a49d000fe
חנה רבקה ויא <213363815@mbydomain>
2023-04-19 12:17:52 +0300

    added contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+const Contact = () => {
+    return(
+        <>
+        <h1>רוצים להכיר? דברו איתנו</h1>
+        <label>שם</label>
+        <label>טלפון</label>
+        <label>דוא"ל</label>
+        <textarea>תוכן הפניה</textarea>
+        </>
+    )
+}
+export default Contact;
