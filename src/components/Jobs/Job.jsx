da5d9a4aef9f08afbdd629731c38bec87af2e611
ChaniEsty <36539137033@mby.co.il>
2023-05-01 08:14:07 +0300

    job changed

--- src/components/Jobs/Job.jsx
+++ src/components/Jobs/Job.jsx
@@ -3,15 +3,15 @@ import React from "react";
 import SendCVModal from "./SendCVModel";
 const Job = (props) => {
   const [openModal, setOpenModal] = useState(false);
-  console.log(props.job);
+  console.log(props.job.city.name);
   return (
     <>
       <div>
         <h4>{props.job.name}</h4>
-       <span>{props.job.generalDescription}</span><br></br>
-        <span>עיר:{props.job.city}</span><br></br>
-        <span>תחום:{props.job.field}</span><br></br>
-        <span>מקצוע:{props.job.subject}</span><br></br>  {/* */}
+       <span>{props.job.genralDescription}</span><br></br>
+        <span>עיר:{props.job.city.name}</span><br></br>
+        <span>תחום:{props.job.field.name}</span><br></br>
+        <span>מקצוע:{props.job.subject.name}</span><br></br>   
         </div>
       <button onClick={() => setOpenModal(true)}>שלח קו"ח</button>
       <SendCVModal open={openModal} setOpenModal={setOpenModal} />    </>
